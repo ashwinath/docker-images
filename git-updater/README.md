b2288dcc01ab390d49cca5082d468c50f38cf8b1
Ashwin Chatterji <ashwinath@hotmail.com>
2023-07-05 13:34:06 +0800

    Add git updater.

---
+++ git-updater/README.md
@@ -0,0 +1,12 @@
+# Git sync
+
+A simple implementation to clone and keep a repo updated.
+
+## Env vars
+
+Name | description
+---- | -----------
+FOLDER_DIR | destination to clone into or update into
+BRANCH_NAME | name of the branch
+GIT_URL | only https supported.
+INTERVAL | interval to keep syncing git folder in seconds
---
+++ git-updater/git-sync.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+set -e
+
+[[ -z "${FOLDER_DIR}" ]] || echo "FOLDER_DIR env var not set" || exit 1
+[[ -z "${BRANCH_NAME}" ]] || echo "BRANCH_NAME env var not set" || exit 1
+[[ -z "${GIT_URL}" ]] || echo "GIT_URL env var not set" || exit 1
+[[ -z "${INTERVAL}" ]] || echo "INTERVAL env var not set" || exit 1
+
+while true; do
+    if [ -d "${FOLDER_DIR}" ]; then
+        pushd "{FOLDER_DIR}"
+        {
+            git fetch
+            git checkout -B "${BRANCH_NAME}" "origin/${BRANCH_NAME}"
+        }
+        popd
+    else
+        git clone "${GIT_URL}" "${FOLDER_DIR}"
+        pushd "{FOLDER_DIR}"
+        {
+            git checkout -B "${BRANCH_NAME}" "origin/${BRANCH_NAME}"
+        }
+        popd
+    fi
+    sleep ${INTERVAL}
+done
